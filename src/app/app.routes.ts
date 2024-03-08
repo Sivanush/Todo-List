609c042c93e6f9ad9fdeb154313383cbed7eb2e5
Sivanush <sivanushedapal@gmail.com>
2024-03-08 15:56:23 +0530

    http files created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { AllComponent } from './components/pages/all/all.component';
+import { ImportentComponent } from './components/pages/importent/importent.component';
+import { CompletedComponent } from './components/pages/completed/completed.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component:AllComponent
+    },
+    {
+        path:'importent',
+        component:ImportentComponent
+    },
+    {
+        path:'complete',
+        component:CompletedComponent  
+    }
+];
--- src/app/components/pages/all/all.component.ts
+++ src/app/components/pages/all/all.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-all',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './all.component.html',
   styleUrl: './all.component.scss'
 })
 export class AllComponent {
+  newTask = ''
+
+  addTask(){
+    console.log('Submitted',this.newTask);
+    
+  }
 
 }
